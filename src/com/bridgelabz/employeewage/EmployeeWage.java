573057169ad1a3ef3c1e9e20c5e2a590bb84263e
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-01-17 20:08:14 +0530

    Added: Calculate wages till a condition of total working hours or days is reached

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class EmployeeWage {
 
 	/*
-	 * Calculating Wages for a Month by assuming 20 working day per month
+	 * Calculate Wages till a condition of total working hours or days is reached
 	 */
 	public static void main(String[] args) {
 
@@ -18,39 +18,39 @@ public class EmployeeWage {
 		int halfDayHours = 4;
 		int salary = 0;
 		int monthlysalary = 0;
-		int workingDaysPerMonth = 20;
+		int days = 0;
+		int monthlyHours = 0;
 
 		/*
-		 * using a loop to ask how many salary in working day per monthly will give
-		 * 0,1,2 random number
+		 * for a month assuming 100 hours and 20day will give 0,1,2 random number
 		 */
-		for (int i = 1; i <= workingDaysPerMonth; i++) {
+		while (days != 20 && monthlyHours != 100) {
+			days++;
 			int randomNum = random.nextInt(3);
 
 			/*
-			 * By using Switch case statement Calculate and getting this salary Getting this
-			 * salary employee Present and Absent and print salary
+			 * By using Switch case statement Calculate and getting this salary
 			 */
 			switch (randomNum) {
 
 			case 0:
-				System.out.println("Employee is Absent");
-				System.out.println("Monthly Salary will be: ");
+				// System.out.println("Employee is Absent");
+				// System.out.println("Monthly Salary will be: ");
 				break;
 			case 1:
-				System.out.println("Employee is Present");
+				// System.out.println("Employee is Present for Full Day");
 				System.out.println("Monthly salary for Full Day: ");
 				salary = wagePerHour * fullDayHours;
 				break;
 			case 2:
-				System.out.println("Employee is Present");
+				// System.out.println("Employee is Present for Half Day");
 				System.out.println("Monthly Salary for Half Day: ");
 				salary = wagePerHour * halfDayHours;
 				break;
 
 			}
 			monthlysalary = monthlysalary + salary;
-			System.out.println(+salary);
+			System.out.println("Day" + days + " Salary is: " + salary); // print days and with salary base
 
 		}
 		System.out.println("Monthly Salary: " + monthlysalary); // Print the total of monthly salary
