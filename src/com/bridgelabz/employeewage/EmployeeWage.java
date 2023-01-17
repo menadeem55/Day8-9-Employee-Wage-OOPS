bc26c48d08989bb32d0d47bb53e658744b7c1ba3
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-01-17 19:51:07 +0530

    Added: checking Employee is present or Absent

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -1,5 +1,30 @@
 package com.bridgelabz.employeewage;
 
+import java.util.Random;
+
 public class EmployeeWage {
+
+	/*
+	 * check Employee is present or Absent using random
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Employee Wage Computation Program");
+
+		Random random = new Random(); // random object
+		/*
+		 * will give 0 or 1 random number
+		 */
+		int randomNum = random.nextInt(2);
+
+		/*
+		 * condition for present or absent
+		 */
+		if (randomNum == 1)
+			System.out.println("Employee  is Present");
+		else
+			System.out.println("Employee  is Absent");
+
+	}
 
 }
